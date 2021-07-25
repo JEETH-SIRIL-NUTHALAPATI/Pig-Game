e518d9a5b2aaf12100000bb4d2cd2d1d82947463
JEETH-SIRIL-NUTHLAPATI <jsnpaati@gmail.com>
2021-07-25 10:51:58 +0530

    holding score and switching players.

--- script.js
+++ script.js
@@ -18,8 +18,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling the dice
 btnRoll.addEventListener('click', function () {
@@ -39,10 +48,26 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  //Add current score to active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //check if player score is >=100
+  if (scores[activePlayer] >= 20) {
+    //finish game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    //Switch the player
+    switchPlayer();
   }
 });
